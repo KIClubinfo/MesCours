2d6cdd7b11264bf8dff703c346fdf0d8f9f6672a
Mickaël <mickael@securem.eu>
2015-02-17 23:49:18 +0100

    Update 2015-02-17T22:49:16.983Z --skip-ci

---
+++ pwl.js
@@ -0,0 +1,11 @@
+var _paq = _paq || [];
+_paq.push(["setCookieDomain", "*.http"]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u="//piwik.securem.eu/";
+    _paq.push(['setTrackerUrl', u+'pw.php']);
+    _paq.push(['setSiteId', 5]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'pw.js'; s.parentNode.insertBefore(g,s);
+})();
